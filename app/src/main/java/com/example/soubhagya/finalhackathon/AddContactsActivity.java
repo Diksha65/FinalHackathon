e151794e19368a54d9e9596bffe325d06a9f2897
soubhagya <soubhagya.nayak3.work@gmail.com>
2017-04-02 04:15:41 +0530

    tcuvkbl

--- app/src/main/java/com/example/soubhagya/finalhackathon/AddContactsActivity.java
+++ app/src/main/java/com/example/soubhagya/finalhackathon/AddContactsActivity.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -13,9 +14,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by soubhagya on 1/4/17.
@@ -77,56 +83,100 @@ public class AddContactsActivity extends AppCompatActivity {
         switch (requestCode) {
             case REQUEST_CONTACT:
                 if (data != null) {
-                    Uri contactUri = data.getData();
-
-                    Cursor c = this.getContentResolver().query(contactUri,
-                            null, null, null, null);
-
-                    if (c.getCount() == 0) {
-                        c.close();
-                        return;
-                    }
-
-                    if (c.moveToFirst()) {
-                        String name = c
-                                .getString(c
-                                        .getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                        String trustedContactNum = c
-                                .getString(c
-                                        .getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
-                        String contactId = c.getString(c
-                                .getColumnIndex(ContactsContract.Contacts._ID));
-                        if (trustedContactNum.equals("1")) {
-                            Cursor phones = this
-                                    .getContentResolver()
-                                    .query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                                            null,
-                                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID
-                                                    + " = " + contactId, null, null);
-                            while (phones.moveToNext()) {
-                                trustedContactNum = phones
-                                        .getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-
-                                notifyUser(trustedContactNum);
-
-                                dataStash.fireBase
-                                            .child("USERS")
-                                            .child(userPhoneNum)
-                                            .child("trustedContacts")
-                                            .child(trustedContactNum)
-                                            .setValue(true);
-                            }
-                            phones.close();
-                        }
-                        Log.d("Stack",
-                                name + " " + " " + trustedContactNum + " "
-                                        + c.getColumnCount());
-                    }
-                    c.close();
-
+                    getTrustedContactAndUpdateFirebase(data);
                 }
                 break;
         }
+    }
+
+    public void getTrustedContactAndUpdateFirebase(Intent data){
+        Uri contactUri = data.getData();
+
+        Cursor c = this.getContentResolver().query(contactUri,
+                null, null, null, null);
+
+        if (c.getCount() == 0) {
+            c.close();
+            return;
+        }
+
+        if (c.moveToFirst()) {
+            String name = c
+                    .getString(c
+                            .getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String trustedContactNum = c
+                    .getString(c
+                            .getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+            String contactId = c.getString(c
+                    .getColumnIndex(ContactsContract.Contacts._ID));
+            if (trustedContactNum.equals("1")) {
+                Cursor phones = this
+                        .getContentResolver()
+                        .query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                                null,
+                                ContactsContract.CommonDataKinds.Phone.CONTACT_ID
+                                        + " = " + contactId, null, null);
+                while (phones.moveToNext()) {
+                    trustedContactNum = phones
+                            .getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                    notifyUser(trustedContactNum);
+
+                    updateUserNode(trustedContactNum);
+
+                    //updateTrustedContactNode(trustedContactNum);
+
+                }
+                phones.close();
+            }
+            Log.d("Stack",
+                    name + " " + " " + trustedContactNum + " "
+                            + c.getColumnCount());
+        }
+        c.close();
+
+    }
+
+    public void updateUserNode(final String trustedContactNum){
+        dataStash.fireBase
+                .child("USERS")
+                .child(userPhoneNum)
+                .child("trustedContacts")
+                .child(trustedContactNum)
+                .setValue(true)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        dataStash.fireBase
+                                .child("USERS")
+                                .child(trustedContactNum)
+                                .child("dependents")
+                                .setValue(userPhoneNum);
+                    }
+                });
+    }
+
+    public void updateTrustedContactNode(final String trustedContactNum){
+        dataStash.fireBase
+                .child("USERS")
+                .child(trustedContactNum)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            dataStash.fireBase
+                                    .child("USERS")
+                                    .child(trustedContactNum)
+                                    .child("dependents")
+                                    .setValue(userPhoneNum);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
     }
 
     public void notifyUser(String message) {
